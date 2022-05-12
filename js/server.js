04629ea5706e847699d5f02e4187ceeab49dbc12
PinkyGoose <lachuginv@list.ru>
2022-05-12 12:21:57 +0300

    Initial

---
+++ js/server.js
@@ -0,0 +1,19 @@
+
+const http =require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (request, response){
+    console.log('Hello!'+request.method+request.url);
+    if(request.url=='/style.css'){
+        const css = fs.readFileSync('style.css','utf8');
+        response.end(css);
+    }
+    else {
+        const text = fs.readFileSync('index.html', 'utf8');
+        response.end(text);
+    }
+});
+
+
+server.listen(process.env.PORT||3000);
+console.log('Server started!');
